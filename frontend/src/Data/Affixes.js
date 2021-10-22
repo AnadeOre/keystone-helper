ec5bf0711c714b84541361153a71c822d5d280d5
Ana <anaemilia.deorellana@gmail.com>
2021-10-22 01:26:19 -0300

    Rearange Affixes and Talents Data

--- frontend/src/Data/Affixes.js
+++ frontend/src/Data/Affixes.js
@@ -1,182 +1,182 @@
 const Affixes = {
-  "affixes": [
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/1?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Overflowing",
-      "id": 1
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/2?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Skittish",
-      "id": 2
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/3?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Volcanic",
-      "difficulty": "easy",
-      "id": 3
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/4?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Necrotic",
-      "difficulty": "hard",
-      "id": 4
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/5?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Teeming",
-      "id": 5
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/6?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Raging",
-      "difficulty": "med",
-      "id": 6
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/7?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Bolstering",
-      "difficulty": "med",
-      "id": 7
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/8?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Sanguine",
-      "difficulty": "easy",
-      "id": 8
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/9?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Tyrannical",
-      "difficulty": "hard",
-      "id": 9
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/10?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Fortified",
-      "difficulty": "hard",
-      "id": 10
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/11?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Bursting",
-      "difficulty": "med",
-      "id": 11
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/12?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Grievous",
-      "difficulty": "med",
-      "id": 12
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/13?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Explosive",
-      "difficulty": "med",
-      "id": 13
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/14?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Quaking",
-      "difficulty": "easy",
-      "id": 14
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/16?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Infested",
-      "id": 16
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/117?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Reaping",
-      "id": 117
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/119?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Beguiling",
-      "id": 119
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/120?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Awakened",
-      "id": 120
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/121?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Prideful",
-      "difficulty": "med",
-      "id": 121
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/122?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Inspiring",
-      "difficulty": "easy",
-      "id": 122
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/123?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Spiteful",
-      "difficulty": "med",
-      "id": 123
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/124?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Storming",
-      "difficulty": "med",
-      "id": 124
-    },
-    {
-      "key": {
-        "href": "https://us.api.blizzard.com/data/wow/keystone-affix/128?namespace=static-9.1.0_39069-us"
-      },
-      "name": "Tormented",
-      "difficulty": "med",
-      "id": 128
-    }
-  ]
-}
-export default Affixes
+	affixes: [
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/1?namespace=static-9.1.0_39069-us",
+			},
+			name: "Overflowing",
+			id: 1,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/2?namespace=static-9.1.0_39069-us",
+			},
+			name: "Skittish",
+			id: 2,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/3?namespace=static-9.1.0_39069-us",
+			},
+			name: "Volcanic",
+			difficulty: "easy",
+			id: 3,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/4?namespace=static-9.1.0_39069-us",
+			},
+			name: "Necrotic",
+			difficulty: "hard",
+			id: 4,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/5?namespace=static-9.1.0_39069-us",
+			},
+			name: "Teeming",
+			id: 5,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/6?namespace=static-9.1.0_39069-us",
+			},
+			name: "Raging",
+			difficulty: "med",
+			id: 6,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/7?namespace=static-9.1.0_39069-us",
+			},
+			name: "Bolstering",
+			difficulty: "med",
+			id: 7,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/8?namespace=static-9.1.0_39069-us",
+			},
+			name: "Sanguine",
+			difficulty: "easy",
+			id: 8,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/9?namespace=static-9.1.0_39069-us",
+			},
+			name: "Tyrannical",
+			difficulty: "hard",
+			id: 9,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/10?namespace=static-9.1.0_39069-us",
+			},
+			name: "Fortified",
+			difficulty: "hard",
+			id: 10,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/11?namespace=static-9.1.0_39069-us",
+			},
+			name: "Bursting",
+			difficulty: "med",
+			id: 11,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/12?namespace=static-9.1.0_39069-us",
+			},
+			name: "Grievous",
+			difficulty: "med",
+			id: 12,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/13?namespace=static-9.1.0_39069-us",
+			},
+			name: "Explosive",
+			difficulty: "med",
+			id: 13,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/14?namespace=static-9.1.0_39069-us",
+			},
+			name: "Quaking",
+			difficulty: "easy",
+			id: 14,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/16?namespace=static-9.1.0_39069-us",
+			},
+			name: "Infested",
+			id: 16,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/117?namespace=static-9.1.0_39069-us",
+			},
+			name: "Reaping",
+			id: 117,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/119?namespace=static-9.1.0_39069-us",
+			},
+			name: "Beguiling",
+			id: 119,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/120?namespace=static-9.1.0_39069-us",
+			},
+			name: "Awakened",
+			id: 120,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/121?namespace=static-9.1.0_39069-us",
+			},
+			name: "Prideful",
+			difficulty: "med",
+			id: 121,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/122?namespace=static-9.1.0_39069-us",
+			},
+			name: "Inspiring",
+			difficulty: "easy",
+			id: 122,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/123?namespace=static-9.1.0_39069-us",
+			},
+			name: "Spiteful",
+			difficulty: "med",
+			id: 123,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/124?namespace=static-9.1.0_39069-us",
+			},
+			name: "Storming",
+			difficulty: "med",
+			id: 124,
+		},
+		{
+			key: {
+				href: "https://us.api.blizzard.com/data/wow/keystone-affix/128?namespace=static-9.1.0_39069-us",
+			},
+			name: "Tormented",
+			difficulty: "med",
+			id: 128,
+		},
+	],
+};
+export default Affixes;
