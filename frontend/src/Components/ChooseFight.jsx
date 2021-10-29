138bd591f79faeac6431952ea1642d49508a0d03
Ana <anaemilia.deorellana@gmail.com>
2021-10-29 12:07:53 -0300

    Add support for multiple dungeon reports

---
+++ frontend/src/Components/ChooseFight.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { Summary } from './Summary';
+import '../Styles/ChooseFight.css';
+
+export const ChooseFight = ({ props }) => {
+  const history = useHistory();
+  const [hasChosen, setHasChosen] = useState(null);
+  const [dataRecieved, setDataRecieved] = useState(null);
+  const code = history.location.pathname.substring(1);
+  const report = props.reportData.report;
+  const { fights, title } = report;
+  const [index, setIndex] = useState(null);
+  async function fetchApi(code, id, startTime, endTime) {
+    try {
+      let res = await fetch(
+        `http://localhost:3001/api/summary/${code}/${id}/${startTime}/${endTime}`
+      );
+      let responseIsOK = res && res.ok;
+      if (responseIsOK) {
+        let data = await res.json();
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleFightChoice(code, id, startTime, endTime, index) {
+    const dataFromAPI = await fetchApi(code, id, startTime, endTime);
+    if (!dataFromAPI.error) {
+      setDataRecieved(dataFromAPI);
+      setIndex(index);
+      //console.log(dataFromAPI, index);
+      setHasChosen(true);
+    }
+  }
+  return (
+    <div>
+      {!hasChosen ? (
+        <div className='container'>
+          <h1 className='titleChoose'>Report name: {title}</h1>
+          <div className='fightsContainer'>
+            <h3>Choose a dungeon to analyse:</h3>
+            <div className='fightsSection'>
+              {fights.map((fight, index) => (
+                <div key={index}>
+                  {fight.keystoneLevel ? (
+                    <button
+                      className={
+                        fight.gameZone.name.split(' ').join('').toLowerCase() +
+                        ' choose'
+                      }
+                      key={fight.id}
+                      onClick={() =>
+                        handleFightChoice(
+                          code,
+                          parseInt(fight.encounterID),
+                          parseInt(fight.startTime),
+                          parseInt(fight.endTime),
+                          index
+                        )
+                      }>
+                      {fight.gameZone.name} Level {fight.keystoneLevel}
+                    </button>
+                  ) : (
+                    <div>{fight.gameZone.name} is not a key</div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Summary data={dataRecieved} fightIndex={index} />
+      )}
+    </div>
+  );
+};
--- frontend/src/Components/Summary.jsx
+++ frontend/src/Components/Summary.jsx
@@ -1,118 +1,122 @@
-import React, { useEffect } from "react";
-import { getAffixesNames } from "../Utils/getAffixesNames";
-import getDeathsAmounts from "../Utils/getDeathsAmounts";
-import "../Styles/Summary.css";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import getAffixesNames from '../Utils/getAffixesNames';
+import getDeathsAmounts from '../Utils/getDeathsAmounts';
+import '../Styles/Summary.css';
 
-export const Summary = (props) => {
-	console.log("in summary");
-	useEffect(() => {
-		console.log(props);
-	}, []);
+export const Summary = ({ data, fightIndex }) => {
+  const roundTwoDecimals = num => {
+    return Math.round(num * 100) / 100;
+  };
+  const toMinutes = ms => {
+    return ms / (60 * 1000);
+  };
 
-	const roundTwoDecimals = (num) => {
-		return Math.round(num * 100) / 100;
-	};
-	const toMinutes = (ms) => {
-		return ms / (60 * 1000);
-	};
+  const report = data.reportData.report;
+  const { fights, masterData, region, table, title } = report;
 
-	const { report } = props.props.reportData;
-	const { table, fights, region, title, masterData } = report;
-	const {
-		averageItemLevel,
-		keystoneAffixes,
-		keystoneLevel,
-		keystoneTime,
-		gameZone,
-		dungeonPulls,
-	} = fights[0];
-	const { totalTime, damageDone, healingDone, deathEvents } = table.data;
+  const {
+    averageItemLevel,
+    endTime,
+    startTime,
+    keystoneAffixes,
+    keystoneLevel,
+    keystoneTime,
+    gameZone,
+    dungeonPulls,
+  } = fights[fightIndex];
+  const {
+    composition,
+    totalTime,
+    damageDone,
+    healingDone,
+    damageTaken,
+    deathEvents,
+  } = table.data;
 
-	const dungeonName = gameZone.name;
+  const dungeonName = gameZone.name;
+  // Key times
+  const timeLeft = toMinutes(keystoneTime - totalTime);
+  let keyTimeColor;
+  if (timeLeft < 0) keyTimeColor = 'untimed';
+  else keyTimeColor = 'timed';
 
-	// Key times
-	const timeLeft = toMinutes(keystoneTime - totalTime);
-	let keyTimeColor;
-	if (timeLeft < 0) keyTimeColor = "untimed";
-	else keyTimeColor = "timed";
+  //Affixes
+  const weeklyAffixes = getAffixesNames(keystoneAffixes);
 
-	//Affixes
-	const weeklyAffixes = getAffixesNames(keystoneAffixes);
+  //Deaths
+  const deathsCount = getDeathsAmounts(deathEvents);
+  let deathNumber = 0;
+  deathsCount.forEach(death => (deathNumber = deathNumber + death[1]));
 
-	//Deaths
-	const deathsCount = getDeathsAmounts(deathEvents);
-	let deathNumber = 0;
-	deathsCount.forEach((death) => (deathNumber = deathNumber + death.amount));
-	/*
-	return (
-		<div>
-			<p>estoye n summary</p>
-		</div>
-	);
-	*/
-	return (
-		<div>
-			<h1>
-				{dungeonName} level {keystoneLevel}
-			</h1>
-			<div className='summary-container'>
-				<div className='affixes-div'>
-					<ul className='affixes-ul'>
-						{weeklyAffixes.map((affix) => (
-							<li key={affix.id} className={affix.difficulty}>
-								{affix.name}
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className='general-div'>
-					<ul>
-						<li className='deaths-summary '>
-							<span>Deaths: </span>
-							<span>{deathNumber}</span>
-							<div className='deaths-summary hide'>
-								<ul>
-									{deathsCount.map((death, index) => (
-										<li key={index}>
-											<span>{death.name}: </span>
-											{death.amount}
-										</li>
-									))}
-								</ul>
-							</div>
-						</li>
-						<li className='av-ilvl'>
-							<span>Average Item Level: </span>
-							<span>{roundTwoDecimals(averageItemLevel)}</span>
-						</li>
-					</ul>
-				</div>
-				<div className='times-div'>
-					<ul>
-						<li>
-							<span>Time: </span>
-							<span className={keyTimeColor}>
-								{roundTwoDecimals(toMinutes(totalTime))}
-							</span>
-						</li>
-						<li>
-							<span>Key time: </span>
-							<span>{keystoneTime}</span>
-						</li>
-						<li>
-							<span>Time left: </span>
-							<span>{timeLeft}</span>
-							<span>stars</span>
-						</li>
-					</ul>
-				</div>
-				<div className='damage-done'>
-					<h4>Damage Done</h4> <ul></ul>
-				</div>
-				<div className='healing-done'>
-					<h4>Healing Done</h4> <ul></ul>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>
+            {dungeonName} level {keystoneLevel}
+          </h1>
+          <div className='summary-container'>
+            <div className='affixes-div'>
+              {
+                <ul className='affixes-ul'>
+                  {weeklyAffixes.map(affix => (
+                    <li key={affix.id} className={affix.difficulty}>
+                      {affix.name}
+                    </li>
+                  ))}
+                </ul>
+              }
+            </div>
+            <div className='general-div'>
+              <ul>
+                <li className='deaths-summary '>
+                  <span>Deaths: </span>
+                  <span>{deathNumber}</span>
+                  <div className='deaths-summary hide'>
+                    <ul>
+                      {deathsCount.map((death, index) => (
+                        <li key={index}>
+                          <span>{death[0]}: </span>
+                          {death[1]}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+                <li className='av-ilvl'>
+                  <span>Average Item Level: </span>
+                  <span>{roundTwoDecimals(averageItemLevel)}</span>
+                </li>
+              </ul>
+            </div>
+            <div className='times-div'>
+              <ul>
+                <li>
+                  <span>Time: </span>
+                  <span className={keyTimeColor}>
+                    {roundTwoDecimals(toMinutes(totalTime))}
+                  </span>
+                </li>
+                <li>
+                  <span>Key time: </span>
+                  <span>{roundTwoDecimals(toMinutes(keystoneTime))}</span>
+                </li>
+                <li>
+                  <span>Time left: </span>
+                  <span>{roundTwoDecimals(timeLeft)} </span>
+                </li>
+              </ul>
+            </div>
+            <div className='damage-done'>
+              <h4>Damage Done</h4> <ul></ul>
+            </div>
+            <div className='healing-done'>
+              <h4>Healing Done</h4> <ul></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
